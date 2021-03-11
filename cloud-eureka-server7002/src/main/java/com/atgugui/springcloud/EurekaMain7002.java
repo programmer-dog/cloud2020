a63944f8aece5db2b7faf15d072845024d5ebf71
gaozc <gaozc@epsoft.com.cn>
2021-03-11 16:47:34 +0800

    服务注册集群

---
+++ cloud-eureka-server7002/src/main/java/com/atgugui/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.atgugui.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @描述
+ * @创建人 gaozc
+ * @创建时间 $
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
---
+++ cloud-eureka-server7003/src/main/java/com/atguigu/springcloud/EurekaMain7003.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @描述
+ * @创建人 gaozc
+ * @创建时间 $
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7003 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7003.class,args);
+    }
+}
